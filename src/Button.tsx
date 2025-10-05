7f8cca27fa673b6afaee361d635c274875615807
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-10-05 14:59:03 +0200

    add disabled for Btn

--- src/Button.tsx
+++ src/Button.tsx
@@ -1,9 +1,15 @@
 type BtnTypes = {
     title: string
     onClick: () => void
+    isDisabled?: boolean
 }
 
-export const Button = ({title, onClick}: BtnTypes) => {
-    return <button onClick={onClick}>{title}</button>
+export const Button = ({title, onClick, isDisabled}: BtnTypes) => {
+    return (
+        <button disabled={isDisabled}
+                onClick={onClick}>
+            {title}
+        </button>
+    )
 };
 
